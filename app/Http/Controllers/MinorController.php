7130025daf4f207193c94e646f543a5fe62cb5af
timyboy12345 <t.j.m.arendsen@gmail.com>
2019-04-03 11:40:11 +0200

    Filter panel moved

--- app/Http/Controllers/MinorController.php
+++ app/Http/Controllers/MinorController.php
@@ -9,7 +9,7 @@ class MinorController extends Controller
 {
     public function List(Route $route)
     {
-        $minors = array(new Minor(["id" => 1, "name" => "Minor 1"]), new Minor(["id" => 2, "name" => "Minor 2"]));
+        $minors = array(new Minor(["id" => 1, "name" => "Minor 1"]), new Minor(["id" => 2, "name" => "Minor 2"]), new Minor(["id" => 2, "name" => "Minor 2"]), new Minor(["id" => 2, "name" => "Minor 2"]), new Minor(["id" => 2, "name" => "Minor 2"]), new Minor(["id" => 2, "name" => "Minor 2"]));
 
         return view('minors/list', ["minors" => $minors]);
     }
