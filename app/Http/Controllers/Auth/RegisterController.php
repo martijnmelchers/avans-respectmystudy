28bef3005c4820eb4e3ade22569606e49a2b215d
timyboy12345 <t.j.m.arendsen@gmail.com>
2019-04-07 19:05:32 +0200

    Account creation fixes - Role id and username

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,6 +67,8 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'role_id' => 1,
+            'username' => ""
         ]);
     }
 }
