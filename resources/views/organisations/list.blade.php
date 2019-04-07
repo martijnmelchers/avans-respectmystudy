5a50705cdbca76a051477f5dcd90f72689e3aa49
Vincent Bouwman <32157301+TequilaTime@users.noreply.github.com>
2019-04-07 21:03:26 +0200

    Update list.blade.php

--- resources/views/organisations/list.blade.php
+++ resources/views/organisations/list.blade.php
@@ -4,18 +4,6 @@
 
 @section('content')
     <div class="content">
-        {{--<div class="sidebar">--}}
-            {{--<h3 class="center">Filter organisaties</h3>--}}
-            {{--<p>{{sizeof($organisations)}} organisaties gevonden</p>--}}
-
-            {{--<form method="get" class="form" autocomplete="off">--}}
-                {{--<div class="formline wrap">--}}
-                    {{--<label for="name" class="wide">Naam</label>--}}
-                    {{--<input type="text" id="name" name="name" placeholder="Naam"></div>--}}
-                {{--<input type="submit">--}}
-            {{--</form>--}}
-        {{--</div>--}}
-
         <div class="wrapper wrap">
             <article>
                 <h1>Alle organisaties</h1>
