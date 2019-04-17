e1f04343a098964c3ff8eb297da629e74aff2b0e
Vincent Bouwman <32157301+TequilaTime@users.noreply.github.com>
2019-04-17 10:12:11 +0200

    homepage V2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,7 +29,7 @@
                         <form method="get" autocomplete="off" action="minors">
                             <p class="info">Zoek hier in onze database met minors.</p>
                             <div class="formline">
-                                <input name="name" placeholder="vul een zoekopdracht in.." type="text">
+                                <input name="name" placeholder="Vul een zoekopdracht in..." type="text">
                             </div>
                             {{--<div class="searcharticlefilter">--}}
                             {{--<div class="articlefill"></div>--}}
@@ -44,10 +44,10 @@
                     </div>
                 </div>
             </div>
-            <div class="infogroup">
+            <div class="articlegroup">
                 <article>
                     <div class="infopoint">
-                        <span class="fas fa-tasks fa-6x"></span>
+                        <span class="fas fa-tasks fa-5x"></span>
                         <div class="infopointtext">
                             <h3>Quality</h3>
                             <h3>Control</h3>
@@ -64,13 +64,13 @@
                     </p>
                     <div class="infobutton">
                         <div class="buttons">
-                            <a href="/" class="button aqua">Lees meer</a>
+                            <a href="/" class="button blue">Lees meer</a>
                         </div>
                     </div>
                 </article>
                 <article>
                     <div class="infopoint">
-                        <span class="fas fa-star fa-6x"></span>
+                        <span class="fas fa-star fa-5x"></span>
                         <div class="infopointtext">
                             <h3>Uniek</h3>
                             <h3>Systeem</h3>
@@ -86,13 +86,13 @@
                     </p>
                     <div class="infobutton">
                         <div class="buttons">
-                            <a href="/" class="button aqua">Lees meer</a>
+                            <a href="/" class="button blue">Lees meer</a>
                         </div>
                     </div>
                 </article>
                 <article>
                     <div class="infopoint">
-                        <span class="fas fa-filter fa-6x"></span>
+                        <span class="fas fa-filter fa-5x"></span>
                         <div class="infopointtext">
                             <h3>Nauwkeurig</h3>
                             <h3>Filteren</h3>
@@ -108,12 +108,92 @@
                     </p>
                     <div class="infobutton">
                         <div class="buttons">
-                            <a href="/" class="button aqua">Lees meer</a>
+                            <a href="/" class="button blue">Lees meer</a>
                         </div>
                     </div>
                 </article>
             </div>
 
+            <div class="articlegroup">
+                <div class="icontitle">
+                <div class="infopoint">
+                    <span class="fas fa-bullhorn fa-5x"></span>
+                    <div class="infopointtext">
+                        <h3>Uitgelichte minoren</h3>
+                    </div>
+                </div>
+                </div>
+                <article>
+                    <p>
+                    </p>
+                </article>
+                <article>
+                    <p>
+                        <span class="far fa-image fa-4x"></span>
+                    </p>
+                </article>
+                <article>
+                    <p>
+                    </p>
+                </article>
+            </div>
+
+            <div class="articlegroup">
+                <div class="icontitle">
+                    <div class="infopoint">
+                        <span class="fas fa-newspaper fa-5x"></span>
+                        <div class="infopointtext">
+                            <h3>Het laatste nieuws</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="newsgroup">
+                <article>
+                    <img src="http://127.0.0.1:8000/svg/404.svg">
+                    <div class="newsgroupcontent">
+                    <h1>Studentenwoningen worden steeds duurder</h1>
+                        <h2>Geschreven door: Juliet de boer | Geplaatst op 5 april 2019</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin facilisis sodales libero, nec
+                        euismod nunc feugiat in.
+                        Donec luctus mi scelerisque felis viverra, eu elementum tellus semper. Sed a tincidunt mauris.
+                        Integer mattis in neque vitae suscipit. Sed sagittis, ante ac gravida aliquet, nisl lectus
+                        pulvinar odio, sed sagittis urna neque vel arcu.
+                        Nunc pulvinar velit justo, sit amet sodales.
+                    </p>
+                        <div class="infobutton">
+                            <div class="buttons">
+                                <a href="/" class="button blue">Verder lezen</a>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+                    <article>
+                        <img src="http://127.0.0.1:8000/svg/404.svg">
+                        <div class="newsgroupcontent">
+                            <h1>Respectmystudy beste site van het jaar</h1>
+                            <h2>Geschreven door: Juliet de boer | Geplaatst op 1 april 2019</h2>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin facilisis sodales libero, nec
+                                euismod nunc feugiat in.
+                                Donec luctus mi scelerisque felis viverra, eu elementum tellus semper. Sed a tincidunt mauris.
+                                Integer mattis in neque vitae suscipit. Sed sagittis, ante ac gravida aliquet, nisl lectus
+                                pulvinar odio, sed sagittis urna neque vel arcu.
+                                Nunc pulvinar velit justo, sit amet sodales.
+                            </p>
+                            <div class="infobutton">
+                                <div class="buttons">
+                                    <a href="/" class="button blue">Verder lezen</a>
+                                </div>
+                            </div>
+                        </div>
+                    </article>
+            </div>
+
+
+            </div>
+
+        </div>
         </div>
     </div>
     </div>
