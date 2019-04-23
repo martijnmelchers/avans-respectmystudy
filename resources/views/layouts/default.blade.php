3213df49b76f80611725d986744bba52bea3c0f2
timyboy12345 <t.j.m.arendsen@gmail.com>
2019-04-23 18:07:32 +0200

    Revert "Fontawesome"
    This reverts commit b94d1dda06556796ec89b0de971bb57eda37d80c.

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -13,9 +13,11 @@
 
     {{--<link href="https://cdn.wordquest.nl/style/wordstrap-gen.css" type="text/css" rel="stylesheet">--}}
 
-    <script src="/js/jquery-3.4.0.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+            integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+            crossorigin="anonymous"></script>
 
-    <link rel="stylesheet" href="/fontawesome/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     {{--<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">--}}
 
     @yield('head')
