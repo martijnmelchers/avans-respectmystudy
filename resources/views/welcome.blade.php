242df6703a07022027f2fbcfe412b906535c8e80
Vincent Bouwman <32157301+TequilaTime@users.noreply.github.com>
2019-05-10 20:16:07 +0200

    Homepage 75% responsive
    Alvast op de branch zodat het bekeken kan worden!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,18 +10,17 @@
         <div class="wrapper wrap">
             <div class="presentation">
                 <div class="row justify-content-center">
-                    <div class="col-xl-4 presentation-left">
+                    <div class="col-xl-4 col-10 presentation-left">
                         <h1>{{__('content.homepage.mainarticletitle')}}</h1>
                         <p>
                             {{__('content.homepage.mainarticlecontent')}}
                         </p>
                     </div>
-                    <div class="col-xl-4 offset-xl-1">
+                    <div class="col-xl-4 col-10 offset-xl-1">
                         <div class="box shadow">
                             <h1> {{__('content.homepage.search.part1')}}
                                 <b>1482</b> {{__('content.homepage.search.part2')}}</h1>
                             <form method="get" autocomplete="off" action="minors">
-                                <p class="info">Zoek hier in onze database met minors.</p>
                                 <div class="formline">
                                     <input name="name" placeholder=" {{__('content.homepage.searchplaceholder')}}"
                                            type="text">
@@ -32,8 +31,9 @@
                                 {{--<h3>Open filters</h3>--}}
                                 {{--</div>--}}
                                 <div class="row justify-content-end">
-                                    <div class="col-xl-4">
-                                        <a href="/" class="button blue"> {{__('content.buttons.searchbutton')}}</a>
+                                    <div class="col-xl-8 col-auto"></div>
+                                    <div class="col-xl-auto col-auto">
+                                        <a href="/" class="button blue "> {{__('content.buttons.searchbutton')}}</a>
                                     </div>
                                 </div>
                             </form>
@@ -41,13 +41,13 @@
                     </div>
                 </div>
             </div>
-            <div class="row justify-content-center highlighted-points">
-                <div class="col-3">
+            <div class="row justify-content-center  highlighted-points ">
+                <div class="col-xl-3 col-10">
                     <div class="row highlight-header">
-                        <div class="col-3">
+                        <div class="col-xl-auto col-auto">
                             <i class="fas fa-tasks fa-6x"></i>
                         </div>
-                        <div class="col-9">
+                        <div class="col-xl-auto col-auto">
                             <h1> {{__('content.homepage.infopoin1ttitle1')}}</h1>
                             <h1> {{__('content.homepage.infopoint1ttitle2')}}</h1>
                         </div>
@@ -55,10 +55,12 @@
                     <div class="row highlight-content">
                         <div class="col-12">
                             <p class="c-secondary f-primary">
-                                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-                                invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
-                                accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-                                sanctus est Lorem ipsum dolor sit amet. Lorem ipsum…
+                            {{__('content.homepage.infopointcontent1')}}
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                             {{__('content.homepage.infopointcontent1')}}
                             </p>
                         </div>
                     </div>
@@ -69,23 +71,25 @@
                     </div>
 
                 </div>
-                <div class="col-3">
+                <div class="col-xl-3 col-10">
                     <div class="row highlight-header">
-                        <div class="col-3">
+                        <div class="col-xl-auto col-auto">
                             <i class="fas fa-star fa-6x"></i>
                         </div>
-                        <div class="col-9">
-                            <h1> {{__('content.homepage.infopoin1ttitle1')}}</h1>
-                            <h1> {{__('content.homepage.infopoint1ttitle2')}}</h1>
+                        <div class="col-xl-auto col-auto">
+                            <h1> {{__('content.homepage.infopoint2ttitle1')}}</h1>
+                            <h1> {{__('content.homepage.infopoint2ttitle2')}}</h1>
                         </div>
                     </div>
                     <div class="row highlight-content">
                         <div class="col-12">
                             <p class="c-secondary f-primary">
-                                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-                                invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
-                                accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-                                sanctus est Lorem ipsum dolor sit amet. Lorem ipsum…
+                            {{__('content.homepage.infopointcontent2')}}
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                            {{__('content.homepage.infopointcontent2')}}
                             </p>
                         </div>
                     </div>
@@ -96,23 +100,25 @@
                     </div>
 
                 </div>
-                <div class="col-3">
+                <div class="col-xl-3 col-10">
                     <div class="row highlight-header">
-                        <div class="col-3">
+                        <div class="col-xl-auto col-auto">
                             <i class="fas fa-filter fa-6x"></i>
                         </div>
-                        <div class="col-9">
-                            <h1> {{__('content.homepage.infopoin1ttitle1')}}</h1>
-                            <h1> {{__('content.homepage.infopoint1ttitle2')}}</h1>
+                        <div class="col-xl-auto col-auto">
+                            <h1> {{__('content.homepage.infopoint3ttitle1')}}</h1>
+                            <h1> {{__('content.homepage.infopoint3ttitle2')}}</h1>
                         </div>
                     </div>
                     <div class="row highlight-content">
                         <div class="col-12">
                             <p class="c-secondary f-primary">
-                                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-                                invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
-                                accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-                                sanctus est Lorem ipsum dolor sit amet. Lorem ipsum…
+                                {{__('content.homepage.infopointcontent3')}}
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                {{__('content.homepage.infopointcontent3')}}
                             </p>
                         </div>
                     </div>
@@ -126,12 +132,12 @@
             </div>
 
             <div class="row highlighted-minors justify-content-center">
-                <div class="col-9">
+                <div class="col-xl-9 col-10">
                     <div class="row highlight-header">
-                        <div class="col-1">
+                        <div class="col-auto">
                             <i class="fas fa-bullhorn fa-4x"></i>
                         </div>
-                        <div class="col-10">
+                        <div class="col-auto">
                             <h1>{{__('content.homepage.highlightedminortitle')}}</h1>
                         </div>
 
@@ -139,33 +145,32 @@
                 </div>
             </div>
             <div class="row highlighted-minors justify-content-center">
-                <div class="col-3">
+                <div class="col-xl-3 col-10">
                     <div class="box shadow">
                         Content
                     </div>
                 </div>
 
-                <div class="col-3">
+                <div class="col-xl-3 col-10">
                     <div class="box shadow">
                         Content
                     </div>
                 </div>
 
-                <div class="col-3">
+                <div class="col-xl-3 col-10">
                     <div class="box shadow">
                         Content
                     </div>
                 </div>
-
             </div>
 
             <div class="row highlighted-minors justify-content-center">
-                <div class="col-9">
+                <div class="col-xl-9 col-10">
                     <div class="row highlight-header">
-                        <div class="col-1">
+                        <div class="col-auto">
                             <i class="fas fa-newspaper fa-4x"></i>
                         </div>
-                        <div class="col-10">
+                        <div class="col-auto">
                             <h1>{{__('content.homepage.newstitle')}}</h1>
                         </div>
 
@@ -173,7 +178,7 @@
                 </div>
             </div>
             <div class="row justify-content-center news-articles">
-                <div class="col-9 article-wrapper">
+                <div class="col-xl-9 col-10 article-wrapper">
                     <div class="box no-padding article">
                         <div class="row">
                             <div class="col-4 article-image">
@@ -183,12 +188,7 @@
                                 <h1 class="w-700">Studentenwoningen worden steeds duurder</h1>
                                 <h6 class="w-300">Geschreven door: Juliet de boer | Geplaatst op 5 april 2019</h6>
                                 <p class="intro-content c-primary f-primary w-400">
-                                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                                    tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero
-                                    eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-                                    takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
-                                    consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-                                    dolore magna…
+                                    Voorbeeld!
                                 </p>
                                 <div class="article-actions text-right">
                                     <a href="/" class="button blue">{{__('content.buttons.readfurtherbutton')}}</a>
@@ -198,7 +198,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-9 article-wrapper">
+                <div class="col-xl-9 col-10 article-wrapper">
                     <div class="box no-padding article">
                         <div class="row">
                             <div class="col-4 article-image">
