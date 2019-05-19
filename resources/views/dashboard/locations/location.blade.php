10876898466d4fae9251e722a3a1314c5fb71a7b
timyboy12345 <t.j.m.arendsen@gmail.com>
2019-05-19 15:37:55 +0200

    Update location.blade.php

--- resources/views/dashboard/locations/location.blade.php
+++ resources/views/dashboard/locations/location.blade.php
@@ -4,16 +4,25 @@
 
 @section('content')
     <div class="row">
-        <article>
+        <div class="col-12 box">
             <h1>{{$location->name}}</h1>
             <ul>
                 <li>{{$location->organisation->name}}</li>
             </ul>
-        </article>
+        </div>
 
-        <div class="buttons">
-            <a href="{{route('dashboard-locations')}}">Alle locaties</a>
-            <a href="{{route('dashboard-organisation', $location->organisation->id)}}">Meer
+        <div class="col-12 box">
+            <h6>Alle minors op deze locatie</h6>
+        </div>
+        <div class="blocks">
+            @foreach ($location->minors as $minor)
+                <a href="{{route('dashboard-minor', $minor->id)}}" class="item p-2">{{$minor->name}}</a>
+            @endforeach
+        </div>
+
+        <div class="buttons mt-2">
+            <a class="button blue" href="{{route('dashboard-locations')}}">Alle locaties</a>
+            <a class="button blue" href="{{route('dashboard-organisation', $location->organisation->id)}}">Meer
                 over {{$location->organisation->name}}</a>
         </div>
     </div>
