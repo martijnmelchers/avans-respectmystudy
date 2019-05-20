9398d5b86ed9a575b0b13649c44efeb382f89173
Vincent Bouwman <32157301+TequilaTime@users.noreply.github.com>
2019-05-20 16:22:51 +0200

    Added organisatie pagina taal support
    bruh

--- resources/lang/en/organisations.php
+++ resources/lang/en/organisations.php
@@ -3,4 +3,13 @@
 return [
     'organisations_title' => 'All organisations',
     'organisations_description' => 'Below are all organizations that offer minors through our site.',
+
+    'all_locations' => 'All locations for',
+    'all_minors' => 'All minors for',
+    'no_minors_found' => 'We have not found any locations for',
+
+    'buttons' => [
+        'home_button' => 'Home',
+        'organisations_button' => 'All organisations',
+    ]
 ];
--- resources/lang/nl/organisations.php
+++ resources/lang/nl/organisations.php
@@ -3,4 +3,13 @@
 return [
     'organisations_title' => 'Alle organisaties',
     'organisations_description' => 'Hieronder staan alle organisaties die minors aanbieden via onze site.',
+
+    'all_locations' => 'Alle locaties van',
+    'all_minors' => 'Alle minors van',
+    'no_minors_found' => 'We hebben geen minors gevonden voor',
+
+    'buttons' => [
+        'home_button' => 'Home',
+        'organisations_button' => 'Alle organisaties',
+    ]
 ];
