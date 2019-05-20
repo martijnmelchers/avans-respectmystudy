c148f840da39000030eccc2dd9112cbf86406ac0
Dennis <me@dennis.dev>
2019-05-20 04:26:56 +0200

    move inline review delete overlay to scss

--- resources/views/minors/minor.blade.php
+++ resources/views/minors/minor.blade.php
@@ -146,36 +146,6 @@
                 <input type="submit" value="Plaats review">
             </form>
         </div>
-        <style>
-            #overlay {
-                position: fixed;
-                display: none;
-                flex: 1;
-                top: 0;
-                left: 0;
-                right: 0;
-                bottom: 0;
-                background-color: rgba(0, 0, 0, 0.5);
-                z-index: 2;
-                justify-content: center;
-                align-items: center;
-            }
-
-            .overlay-container {
-                max-width: 300px;
-                padding: 12px;
-            }
-
-            .overlay-buttons {
-                display: flex;
-                flex-direction: row;
-                margin-top: 8px;
-            }
-
-            .overlay-buttons .button {
-                margin: 8px;
-            }
-        </style>
         <script>
             function showOverlay(button) {
                 window.currentDeleteReviewForm = button.parentElement;
