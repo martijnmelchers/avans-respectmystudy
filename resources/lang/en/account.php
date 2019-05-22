41e2ff79f593e30534818f019ce9cff1be290a94
Vincent Bouwman <32157301+TequilaTime@users.noreply.github.com>
2019-05-22 19:11:04 +0200

    Waarom was deze pagina nog niet gefixed?!

---
+++ resources/lang/en/account.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'welcome' => 'Welcome',
+    'not_linked' => 'Not yet linked to SURF?',
+
+    'surf_info' => 'Account information from SURF:',
+
+    'buttons' => [
+        'link_button' => 'Link now!',
+    ],
+];
