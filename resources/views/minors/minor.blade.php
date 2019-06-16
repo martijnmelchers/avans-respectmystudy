82e236e23c3ad8afe8c22108ac3d3ecc447dd629
timyboy12345 <t.j.m.arendsen@gmail.com>
2019-06-16 21:11:50 +0200

    1 review per user per minor

--- resources/views/minors/minor.blade.php
+++ resources/views/minors/minor.blade.php
@@ -104,85 +104,88 @@
                     </div>
                 @endif
 
-                <form method="post">
-                    {{ csrf_field() }}
-                    <div class="form-group">
-                        <input class="titlefield" type="text" name="title"
-                               placeholder="{{__('minors.review_title_placeholder')}}">
-                    </div>
-                    <div class="form-group">
+                @if (empty($minor->yourReview()))
+                    <form method="post">
+                        @csrf
+
+                        <div class="form-group">
+                            <input value="{{old('title')}}" class="titlefield" type="text" name="title"
+                                   placeholder="{{__('minors.review_title_placeholder')}}">
+                        </div>
+                        <div class="form-group">
                     <textarea name="message" required placeholder="{{__('minors.review_content_placeholder')}}"
-                              type="message"></textarea>
-                    </div>
-                    <div class="d-flex flex-column flex-md-row justify-content-around stars">
-                        <div class="rating">
-                            <input type="radio" id="star5_1" name="rating_1" value="5"/>
-                            <label class="full"
-                                   for="star5_1"
-                                   title="Awesome - 5 stars"></label>
-                            <input type="radio" id="star4_1" name="rating_1" value="4"/>
-                            <label class="full" for="star4_1"
-                                   title="Pretty good - 4 stars"></label>
-                            <input type="radio" id="star3_1" name="rating_1" value="3"/>
-                            <label class="full" for="star3_1"
-                                   title="Meh - 3 stars"></label>
-                            <input type="radio" id="star2_1" name="rating_1" value="2"/>
-                            <label class="full" for="star2_1"
-                                   title="Kinda bad - 2 stars"></label>
-                            <input type="radio" id="star1_1" name="rating_1" value="1"/>
-                            <label class="full" for="star1_1"
-                                   title="Sucks big time - 1 star"></label>
-                            <b>{{__('minors.review_quality')}}</b>
+                              type="message">{{old('message')}}</textarea>
                         </div>
-                        <div class="rating">
-                            <input type="radio" id="star5_2" name="rating_2" value="5"/>
-                            <label class="full"
-                                   for="star5_2"
-                                   title="Awesome - 5 stars"></label>
-                            <input type="radio" id="star4_2" name="rating_2" value="4"/>
-                            <label class="full"
-                                   for="star4_2"
-                                   title="Pretty good - 4 stars"></label>
-                            <input type="radio" id="star3_2" name="rating_2" value="3"/>
-                            <label class="full"
-                                   for="star3_2"
-                                   title="Meh - 3 stars"></label>
-                            <input type="radio" id="star2_2" name="rating_2" value="2"/>
-                            <label class="full"
-                                   for="star2_2"
-                                   title="Kinda bad - 2 stars"></label>
-                            <input type="radio" id="star1_2" name="rating_2" value="1"/>
-                            <label class="full"
-                                   for="star1_2"
-                                   title="Sucks big time - 1 star"></label>
-                            <b>{{__('minors.review_studiability')}}</b>
+                        <div class="d-flex flex-column flex-md-row justify-content-around stars">
+                            <div class="rating">
+                                <input type="radio" id="star5_1" name="rating_1" value="5"/>
+                                <label class="full"
+                                       for="star5_1"
+                                       title="Awesome - 5 stars"></label>
+                                <input type="radio" id="star4_1" name="rating_1" value="4"/>
+                                <label class="full" for="star4_1"
+                                       title="Pretty good - 4 stars"></label>
+                                <input type="radio" id="star3_1" name="rating_1" value="3"/>
+                                <label class="full" for="star3_1"
+                                       title="Meh - 3 stars"></label>
+                                <input type="radio" id="star2_1" name="rating_1" value="2"/>
+                                <label class="full" for="star2_1"
+                                       title="Kinda bad - 2 stars"></label>
+                                <input type="radio" id="star1_1" name="rating_1" value="1"/>
+                                <label class="full" for="star1_1"
+                                       title="Sucks big time - 1 star"></label>
+                                <b>{{__('minors.review_quality')}}</b>
+                            </div>
+                            <div class="rating">
+                                <input type="radio" id="star5_2" name="rating_2" value="5"/>
+                                <label class="full"
+                                       for="star5_2"
+                                       title="Awesome - 5 stars"></label>
+                                <input type="radio" id="star4_2" name="rating_2" value="4"/>
+                                <label class="full"
+                                       for="star4_2"
+                                       title="Pretty good - 4 stars"></label>
+                                <input type="radio" id="star3_2" name="rating_2" value="3"/>
+                                <label class="full"
+                                       for="star3_2"
+                                       title="Meh - 3 stars"></label>
+                                <input type="radio" id="star2_2" name="rating_2" value="2"/>
+                                <label class="full"
+                                       for="star2_2"
+                                       title="Kinda bad - 2 stars"></label>
+                                <input type="radio" id="star1_2" name="rating_2" value="1"/>
+                                <label class="full"
+                                       for="star1_2"
+                                       title="Sucks big time - 1 star"></label>
+                                <b>{{__('minors.review_studiability')}}</b>
+                            </div>
+                            <div class="rating">
+                                <input type="radio" id="star5_3" name="rating_3" value="5"/>
+                                <label class="full"
+                                       for="star5_3"
+                                       title="Awesome - 5 stars"></label>
+                                <input type="radio" id="star4_3" name="rating_3" value="4"/>
+                                <label class="full"
+                                       for="star4_3"
+                                       title="Pretty good - 4 stars"></label>
+                                <input type="radio" id="star3_3" name="rating_3" value="3"/>
+                                <label class="full"
+                                       for="star3_3"
+                                       title="Meh - 3 stars"></label>
+                                <input type="radio" id="star2_3" name="rating_3" value="2"/>
+                                <label class="full"
+                                       for="star2_3"
+                                       title="Kinda bad - 2 stars"></label>
+                                <input type="radio" id="star1_3" name="rating_3" value="1"/>
+                                <label class="full"
+                                       for="star1_3"
+                                       title="Sucks big time - 1 star"></label>
+                                <b>{{__('minors.review_content')}}</b>
+                            </div>
                         </div>
-                        <div class="rating">
-                            <input type="radio" id="star5_3" name="rating_3" value="5"/>
-                            <label class="full"
-                                   for="star5_3"
-                                   title="Awesome - 5 stars"></label>
-                            <input type="radio" id="star4_3" name="rating_3" value="4"/>
-                            <label class="full"
-                                   for="star4_3"
-                                   title="Pretty good - 4 stars"></label>
-                            <input type="radio" id="star3_3" name="rating_3" value="3"/>
-                            <label class="full"
-                                   for="star3_3"
-                                   title="Meh - 3 stars"></label>
-                            <input type="radio" id="star2_3" name="rating_3" value="2"/>
-                            <label class="full"
-                                   for="star2_3"
-                                   title="Kinda bad - 2 stars"></label>
-                            <input type="radio" id="star1_3" name="rating_3" value="1"/>
-                            <label class="full"
-                                   for="star1_3"
-                                   title="Sucks big time - 1 star"></label>
-                            <b>{{__('minors.review_content')}}</b>
-                        </div>
-                    </div>
-                    <input class="button blue" type="submit" value="{{__('minors.buttons.post_button')}}">
-                </form>
+                        <input class="button blue" type="submit" value="{{__('minors.buttons.post_button')}}">
+                    </form>
+                @endif
             </div>
 
             <div id="overlay">
@@ -195,7 +198,8 @@
 
                         <h3>{{__('minors.review_remove_warning')}}</h3>
                         <div class="d-flex flex-row justify-content-center overlay-buttons">
-                            <div class="button red" onclick="hideOverlay(false)">{{__('minors.review_remove')}}</div>
+                            <div class="button red"
+                                 onclick="hideOverlay(false)">{{__('minors.review_remove')}}</div>
                             <div class="button grey"
                                  onclick="hideOverlay(true)">{{__('minors.review_remove_cancel')}}</div>
                         </div>
@@ -205,23 +209,25 @@
 
             <div class="col-12 box">
                 <h3>Reviews</h3>
-                @foreach($reviews as $r)
-                    <div class="box shadow mb-2 review-detail">
-                        @if($r->user_id == $user_id)
-                            <form method="POST">
-                                {{ csrf_field() }}
-                                <input type="hidden" name="_method" value="DELETE"/>
-                                <input type="hidden" name="review" value="{{$r->id}}"/>
-                                <span class="closebutton dark" onclick="showOverlay(this)">&times;</span>
-                            </form>
-                        @endif
-                        <h5>{{__('minors.review_minor_title')}}</h5>
-                        <p>{{$r->description}}</p>
-                        <h5>{{__('minors.review_minor_comment')}}</h5>
-                        <p>{{$r->comment}}</p>
-                        <h5>{{__('minors.review_minor_rating')}}</h5>
-                        <div class="d-flex flex-column flex-md-row justify-content-around stars">
-                            <p class="d-flex flex-column">
+            </div>
+
+            @foreach($reviews as $r)
+                <div class="box shadow mb-2 review-detail">
+                    @if($r->user_id == $user_id)
+                        <form method="POST">
+                            {{ csrf_field() }}
+                            <input type="hidden" name="_method" value="DELETE"/>
+                            <input type="hidden" name="review" value="{{$r->id}}"/>
+                            <span class="closebutton dark" onclick="showOverlay(this)">&times;</span>
+                        </form>
+                    @endif
+                    <h5>{{__('minors.review_minor_title')}}</h5>
+                    <p>{{$r->description}}</p>
+                    <h5>{{__('minors.review_minor_comment')}}</h5>
+                    <p>{{$r->comment}}</p>
+                    <h5>{{__('minors.review_minor_rating')}}</h5>
+                    <div class="d-flex flex-column flex-md-row justify-content-around stars">
+                        <p class="d-flex flex-column">
                                 <span class="d-flex flex-row justify-content-center">
                                     @for($i=0; $i<5; $i++)
                                         @if($i < $r->grade_quality)
@@ -231,10 +237,10 @@
                                         @endif
                                     @endfor
                                 </span>
-                                <b>{{__('minors.review_quality')}}</b>
-                                <span class="description">{{$r->grade_quality}} {{__('minors.review_stars')}}</span>
-                            </p>
-                            <p class="d-flex flex-column">
+                            <b>{{__('minors.review_quality')}}</b>
+                            <span class="description">{{$r->grade_quality}} {{__('minors.review_stars')}}</span>
+                        </p>
+                        <p class="d-flex flex-column">
                                 <span class="d-flex flex-row justify-content-center">
                                     @for($i=0; $i<5; $i++)
                                         @if($i < $r->grade_studiability)
@@ -245,10 +251,10 @@
                                     @endfor
                                 </span>
 
-                                <b class="text-center">{{__('minors.review_studiability')}}</b>
-                                <span class="description">{{$r->grade_studiability}} {{__('minors.review_stars')}}</span>
-                            </p>
-                            <p class="d-flex flex-column">
+                            <b class="text-center">{{__('minors.review_studiability')}}</b>
+                            <span class="description">{{$r->grade_studiability}} {{__('minors.review_stars')}}</span>
+                        </p>
+                        <p class="d-flex flex-column">
 
                                 <span class="d-flex flex-row justify-content-center">
                                     @for($i=0; $i<5; $i++)
@@ -260,27 +266,27 @@
                                     @endfor
                                 </span>
 
-                                <b class="text-center">{{__('minors.review_content')}}</b>
-                                <span class="description">{{$r->grade_content}} {{__('minors.review_stars')}}</span>
-                            </p>
-                        </div>
+                            <b class="text-center">{{__('minors.review_content')}}</b>
+                            <span class="description">{{$r->grade_content}} {{__('minors.review_stars')}}</span>
+                        </p>
                     </div>
-                @endforeach
-            </div>
+                </div>
+            @endforeach
         </div>
+    </div>
 
-        <script>
-            function showOverlay(button) {
-                window.currentDeleteReviewForm = button.parentElement;
-                document.getElementById('overlay').style.display = 'flex';
-            }
+    <script>
+        function showOverlay(button) {
+            window.currentDeleteReviewForm = button.parentElement;
+            document.getElementById('overlay').style.display = 'flex';
+        }
 
-            function hideOverlay(cancel) {
-                if (!cancel && window.currentDeleteReviewForm) {
-                    window.currentDeleteReviewForm.submit();
-                }
-                document.getElementById('overlay').style.display = 'none';
+        function hideOverlay(cancel) {
+            if (!cancel && window.currentDeleteReviewForm) {
+                window.currentDeleteReviewForm.submit();
             }
-        </script>
+            document.getElementById('overlay').style.display = 'none';
+        }
+    </script>
     </div>
 @endsection
